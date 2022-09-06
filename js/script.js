e4d7cd15e3bcd3614d81f3f24baa6837f5e0a6da
kchh1213 <kchh1213@naver.com>
2022-09-07 04:32:55 +0900

    20220907

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
-$(document).ready(function () {
-    
+$(document).ready(function(){
+
+    // document.querySelector('video').addEventListener('ended', myHandler, false);
+
+    // function myHandler(e) {
+    //     console.log('ended');
+    //     setTimeout(function () {
+    //         document.querySelector('video').play();
+    //     }, 10000);
+    // }
+
+
     AOS.init();
 
 
-    
 });
