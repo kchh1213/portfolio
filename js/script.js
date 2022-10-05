e27ae276438b46b44212270d686cbf17de8a612a
kchh1213 <kchh1213@naver.com>
2022-10-05 19:44:34 +0900

    save

--- js/script.js
+++ js/script.js
@@ -1,7 +1,37 @@
 $(document).ready(function () {
-    setTimeout(function () {
-        $(".skills").addClass("active");
-    }, 1000);
+    console.clear();
+
+    $(window).scroll(function () {
+        let scroll = $(this).scrollTop();
+
+        if (scroll > 2000) {
+            $(".skills").addClass("active");
+        }
+    });
+
+    $(".arrow-box").click(function () {
+        window.scrollTo({ left: 0, top:20000, behavior: "smooth" });
+    })
+
+    $(".home").click(function () {
+        window.scrollTo({ left: 0, top:0, behavior: "smooth" });
+    })
+
+    $(".about").click(function () {
+        window.scrollTo({ left: 0, top:1280, behavior: "smooth" });
+    })
+
+    $(".port").click(function () {
+        window.scrollTo({ left: 0, top: 3250, behavior: "smooth" });
+    })
+
+    $(".other").click(function () {
+        window.scrollTo({ left: 0, top:6600, behavior: "smooth" });
+    })
+
+    $(".contact").click(function () {
+        window.scrollTo({ left: 0, top:20000, behavior: "smooth" });
+    })
 
 
     AOS.init();
